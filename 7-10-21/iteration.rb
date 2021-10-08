4b042162e0880616f28ec3d1fb09005abbbf6daa
DigitalDsntHurt <nick.smith@gusto.com>
2021-10-08 07:56:59 -0700

    More ruby iteration exercises

--- 7-10-21/iteration.rb
+++ 7-10-21/iteration.rb
@@ -85,33 +85,3 @@ puts "12. Using the hash you created from the previous exercise, demonstrate how
 
 p contacts["Joe Smith"][:email]
 p contacts["Sally Johnson"][:phone]
-
-puts "\n==== ==== ==== ====\n"
-puts "13. Use Ruby's Array method delete_if and String method start_with? to delete all of the strings that begin with an 's' in the following array."
-
-arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
-
-puts "Then recreate the arr and get rid of all of the strings that start with 's' or starts with 'w'."
-
-puts "\n==== ==== ==== ====\n"
-puts "14. Take the following array and turn it into a new array that consists of strings containing one word. (ex. ['white snow', etc...] → ['white', 'snow', etc...]. Look into using Array's map and flatten methods, as well as String's split method."
-a = ['white snow', 'winter wonderland', 'melting ice', 'slippery sidewalk', 'salted roads', 'white trees']
-
-puts "\n==== ==== ==== ====\n"
-puts "15. What will the following program output?"
-hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
-hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
-
-if hash1 == hash2
-  puts "These hashes are the same!"
-else
-  puts "These hashes are not the same!"
-end
-
-puts "\n==== ==== ==== ====\n"
-puts "16. Challenge: In exercise 11, we manually set the contacts hash values one by one. Now, programmatically loop or iterate over the contacts hash from exercise 11, and populate the associated data from the contact_data array. Hint: you will probably need to iterate over ([:email, :address, :phone]), and some helpful methods might be the Array shift and first methods. Note that this exercise is only concerned with dealing with 1 entry in the contacts hash, like this:\n
-contact_data = ['joe@email.com', '123 Main st.', '555-123-4567']\n
-contacts = {'Joe Smith' => {}}"
-
-puts "\n==== ==== ==== ====\n"
-puts "BONUS. see if you can figure out how to make #16 work with multiple entries in the contacts hash."
---
+++ 8-10-21/README.md
@@ -0,0 +1,24 @@
+# Code Study - October 8, 2021
+
+## Tool
+
+ruby
+
+## Task
+
+ruby iteration exercises: https://launchschool.com/books/ruby/read/intro_exercises
+
+## Purpose
+
+learn about and practice ruby iteration
+
+## Timebox
+
+22 mins
+
+---
+
+## Reflection
+
+Again, fun to do some fundamentals.
+I'm eager to review provided solutions and compare to mine.
