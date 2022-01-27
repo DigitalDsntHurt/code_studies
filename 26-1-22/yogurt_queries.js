f308dc2bc5e2b216ab730a374f19866cab47d618
DigitalDsntHurt <nick.smith@gusto.com>
2022-01-26 19:01:59 -0700

    yogurt reporting

---
+++ 26-1-22/yogurt_queries.js
@@ -0,0 +1,45 @@
+const logs = require('/Users/nick.smith/jot/2022/yoga_practice_journal_2022.json');
+// console.log(logs);
+
+// unique bam classes
+const bams = logs.filter(entry => {
+  return entry.detail.includes('http') && entry.bam === true
+});
+// console.log(bams);
+// console.log('there are', bams.length, 'bam classes');
+
+// get bam classes with a tag
+const getBamClassesByTag = tag => {
+  const payload = logs.filter(entry => {
+    return entry.bam && entry.notes.includes(tag);
+  });
+  console.log(payload);
+  console.log(payload.length, 'bam classes tagged', tag);
+};
+// getBamClassesByTag('flow');
+// getBamClassesByTag('yin');
+
+// get bam classes excluding a tag
+const getBamClassesExcludingTag = tag => {
+  const payload = logs.filter(entry => {
+    return entry.bam && !entry.notes.includes(tag);
+  });
+  // console.log(payload);
+  // console.log(payload.length, 'bam classes not tagged', tag);
+  return payload;
+};
+// getBamClassesExcludingTag('yin');
+
+const displayformattedResults = raw => {
+  const payload = raw.map(entry => {
+    return `${entry.mins} min -- ${entry.notes}
+${entry.detail}`
+  });
+  payload.sort().forEach(thing => {
+    console.log(thing);
+    console.log();
+  });
+};
+
+// console.log(format(getBamClassesExcludingTag('yin')));
+displayformattedResults(getBamClassesExcludingTag('yin'));
