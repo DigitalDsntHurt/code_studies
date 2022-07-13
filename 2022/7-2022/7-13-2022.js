fca847ef9e1b72fd2c8f1b4920fcb157ebc72d87
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2022-07-13 09:00:04 -0700

    date play

---
+++ 2022/7-2022/7-13-2022.js
@@ -0,0 +1,31 @@
+stringDates = ["Jan 9, 2022", "May 8, 2022", "Jul 10, 2022", "May 8, 2022"];
+dateDates = stringDates.map((date) => new Date(date));
+
+stringDates.forEach((date) => {
+  console.log(date);
+});
+
+dateDates.forEach((date) => {
+  console.log(date);
+});
+
+console.log(dateDates[0] > dateDates[1]);
+console.log(dateDates[0] < dateDates[1]);
+
+dateSet = new Set(dateDates);
+console.log(dateSet);
+
+// LEARNING
+// Date objects in javascript are unique
+// each instance of a date object seems to have its own place in memory
+console.log("each date object in javascript has its own place in memory");
+console.log(
+  "this makes it hard to check uniqueness from a list of date objects"
+);
+console.log(new Date("Jan 9, 2022") === new Date("Jan 9, 2022")); // false
+
+// console.error(a);
+// d = new Date();
+// console.log(d);
+// console.log(d);
+// console.log(d + 1);
