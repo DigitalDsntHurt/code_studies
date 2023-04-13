38dcf990dd0cbb9ddbce330bd2e22baac5cb63f4
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-04-13 10:31:04 -0700

    Uses browserify to import xdate into common js file

---
+++ 2023/04-12/browserify_v1/import_xdate.js
@@ -0,0 +1,7 @@
+const xd = require('xdate');
+
+const tradDate = new Date();
+const xDate = new xd();
+
+console.log(tradDate);
+console.log(xDate);
---
+++ 2023/04-12/browserify_v2/index.js
@@ -0,0 +1,5 @@
+const XDate = require('xdate');
+
+const anXDate = new XDate();
+
+console.log(anXDate);
