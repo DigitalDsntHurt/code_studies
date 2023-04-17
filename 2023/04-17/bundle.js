0ed00c6217390b8f095ad177526b61733e08b0b8
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-04-17 16:11:22 -0700

    Updates starting monday and finishing sunday

--- 2023/04-17/bundle.js
+++ 2023/04-17/bundle.js
@@ -2,34 +2,56 @@
 const d3 = require("d3");
 const xdate = require("xdate");
 
+const getWeekMonday = (date) => {
+    if (date.getDay() === 1) return date;
+    while (date.getDay() !== 1) {
+        return getWeekMonday(new xdate(date.addDays(-1)));
+    }
+};
+// console.log(getWeekMonday(new xdate())); // apr 17
+// console.log(getWeekMonday(new xdate(2023, 03, 16))); // apr 10
+
+const getWeekSunday = (date) => {
+    if (date.getDay() === 0) return date;
+    while (date.getDay() !== 0) {
+        return getWeekSunday(new xdate(date.addDays(1)));
+    }
+    
+};
+// console.log(getWeekSunday(new xdate())); // apr 23
+// console.log(getWeekSunday(new xdate(2023, 03, 16))); // apr 16
 
 const createLife = (birthDate, desiredAge) => {
     const life = [];
+    const currentDate = getWeekMonday(birthDate);
     const deathDate = new xdate(new xdate(birthDate).addYears(desiredAge));
+    const endDate = getWeekSunday(deathDate);
 
     let week = [];
-    while (birthDate <= deathDate) {
-        // console.log(birthDate);
-        week.push(new xdate(birthDate));
-        if (birthDate.getDay() === 0) {
+    while (currentDate <= endDate) {
+        console.log(currentDate);
+        week.push(new xdate(currentDate));
+        if (currentDate.getDay() === 0) {
             life.push(week);
             week = [];
         }
-        birthDate.addDays(1);
+        currentDate.addDays(1);
     }
 
     console.log('====');
     console.log(birthDate);
+    console.log(currentDate);
     console.log(desiredAge);
     console.log(deathDate);
+    console.log(endDate);
     return life;
 };
 
 
-const birthDate = new xdate(1990, 03, 08); //.toTimeString();
+const birthDate = new xdate(1990, 03, 08);
 const desiredAge = 111;
-
 console.log(createLife(birthDate, desiredAge));
+
 },{"d3":558,"xdate":567}],2:[function(require,module,exports){
 "use strict";
 
--- 2023/04-17/index.js
+++ 2023/04-17/index.js
@@ -1,31 +1,46 @@
 const d3 = require("d3");
 const xdate = require("xdate");
 
+const getWeekMonday = (date) => {
+    if (date.getDay() === 1) return date;
+    while (date.getDay() !== 1) {
+        return getWeekMonday(new xdate(date.addDays(-1)));
+    }
+};
+// console.log(getWeekMonday(new xdate())); // apr 17
+// console.log(getWeekMonday(new xdate(2023, 03, 16))); // apr 10
+
+const getWeekSunday = (date) => {
+    if (date.getDay() === 0) return date;
+    while (date.getDay() !== 0) {
+        return getWeekSunday(new xdate(date.addDays(1)));
+    }
+    
+};
+// console.log(getWeekSunday(new xdate())); // apr 23
+// console.log(getWeekSunday(new xdate(2023, 03, 16))); // apr 16
 
 const createLife = (birthDate, desiredAge) => {
     const life = [];
+    const currentDate = getWeekMonday(birthDate);
     const deathDate = new xdate(new xdate(birthDate).addYears(desiredAge));
+    const endDate = getWeekSunday(deathDate);
 
     let week = [];
-    while (birthDate <= deathDate) {
-        // console.log(birthDate);
-        week.push(new xdate(birthDate));
-        if (birthDate.getDay() === 0) {
+    while (currentDate <= endDate) {
+        console.log(currentDate);
+        week.push(new xdate(currentDate));
+        if (currentDate.getDay() === 0) {
             life.push(week);
             week = [];
         }
-        birthDate.addDays(1);
+        currentDate.addDays(1);
     }
 
-    console.log('====');
-    console.log(birthDate);
-    console.log(desiredAge);
-    console.log(deathDate);
     return life;
 };
 
 
-const birthDate = new xdate(1990, 03, 08); //.toTimeString();
+const birthDate = new xdate(1990, 03, 08);
 const desiredAge = 111;
-
 console.log(createLife(birthDate, desiredAge));
