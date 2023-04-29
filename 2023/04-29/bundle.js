f9044bbc193c8daf736d57f35dc50744853d54ef
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-04-29 15:04:18 -0700

    Manually makes some nodes and appends them to document

---
+++ 2023/04-29/bundle.js
@@ -0,0 +1,30 @@
+const data = [
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+];
+
+
+console.log(document);
+
+const canvas = document.getElementById('canvas')
+console.log(canvas);
+
+const rect = SVGRectElement 
+console.log(rect);
+
+
+const svg = document.createElement('svg');
+const day = document.createElement('div').setAttribute("class", "daaaaay");
+// day.setAttribute("class", "day")
+canvas.appendChild(svg)
+
+
+// canvas.append('svg')
+// canvas.appendChild("svg")
+
+// canvas.append(rect())
