a947e1ca85aea63a2dd837d16d12d24ebb7d0060
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-05-04 08:49:49 -0700

    Sets up drawing of grid of triangles based on data array

---
+++ 2023/05-04/draw_grid_of_triangles/index.js
@@ -0,0 +1,26 @@
+const d3 = require("d3");
+const colors = require('../../../utils/colors');
+
+const canvas = d3.select('#canvas').append('svg').attr('width', 2000).attr('height', 1500);
+const drawTriangle = (x,y) => {
+    const triangle = d3.symbol().type(d3.symbolTriangle).size(500);
+    canvas
+        .append("path")
+        .attr("d", triangle)
+        .attr("fill", colors.rand())
+        .attr("transform", `translate(${x}, ${y})`);
+};
+// drawTriangle(22,22);
+
+const weeksData = [
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+    [1,2,3,4,5,6,7],
+];
+
+// the below is WIP and does not work
+canvas.data(weeksData).enter(d => drawTriangle());
