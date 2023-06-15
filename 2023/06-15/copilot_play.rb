696d8e1f00a8a166dfacf26953d13d96c10ba290
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-06-15 15:31:23 -0700

    copilot transplates to german for me

---
+++ 2023/06-15/copilot_play.rb
@@ -0,0 +1,6 @@
+# dictionary of english phrases to german
+english_to_german = {
+  "hello" => "hallo",
+  "i miss you, uncle" => "ich vermisse dich, onkel",
+  "maybe i'll figure it all out someday" => "vielleicht werde ich es eines tages herausfinden",
+}
