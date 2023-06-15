05a82c354e4e06f99b57fd8750f48b900fb6cad6
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-06-15 15:39:23 -0700

    writes a method to get monday of a date's current week

--- 2023/06-15/copilot_play.rb
+++ 2023/06-15/copilot_play.rb
@@ -9,3 +9,20 @@ english_to_german = {
 def clean_string(str)
   str.gsub(/[-\s]/, "_")
 end
+
+
+# method accepts a date and returns the date of the monday of that week
+require 'date'
+def get_monday(date)
+  date - date.wday + 1
+end
+
+p get_monday(Date.today)
+
+# what does ruby's date.wday method do?
+# ruby's date.wday method returns the day of the week as an integer, where sunday is 0 and saturday is 6
+
+# re-write the get_monday method in javascript
+# const getMonday = (date) => { 
+#   date.setDate(date.getDate() - date.getDay() + 1)
+# }
