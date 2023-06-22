e2c1a34988829a6f4e1c285184591d62ef3c4cdf
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-06-22 08:24:27 -0700

    Attempts path play

--- 2023/06-22/two/bundle.js
+++ 2023/06-22/two/bundle.js
@@ -718,14 +718,21 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
 
 },{}],3:[function(require,module,exports){
+(function (__dirname){(function (){
 const path = require('path');
 const d3 = require("d3");
 const colors = require('../../../utils/colors');
 const math = require(`../../../utils/math`);
 
 
+// canvas
 const canvas = d3.select("#canvas").append("svg").attr("width", 500).attr("height", 500);
 
+// path play
+const dir = d3.path(__dirname, '');
+console.log(dir);
+
+// sliding shapes
 const startX = math.rand(150, 350);
 const startY = math.rand(150, 350);
 const rectangle = canvas.append('rect')
@@ -737,6 +744,12 @@ const triangle = canvas.append('path')
     .attr('d', `M ${startX} ${startY} L ${startX - 50} ${startY + 75} H ${startX + 50} Z`);
 triangle.transition().duration(math.rand(4444,8888)).attr('transform', `translate(${math.rand(178,222)}, 0)`)
 
+// text play
+canvas.append('text')
+    .attr('x', math.rand(0,400)).attr('y', math.rand(0,400))
+    .text(dir.toString())
+    // .attr('transform', `rotate(${math.rand(0,360)})`)
+}).call(this)}).call(this,"/")
 },{"../../../utils/colors":569,"../../../utils/math":570,"d3":560,"path":1}],4:[function(require,module,exports){
 "use strict";
 
--- 2023/06-22/two/index.js
+++ 2023/06-22/two/index.js
@@ -4,8 +4,14 @@ const colors = require('../../../utils/colors');
 const math = require(`../../../utils/math`);
 
 
+// canvas
 const canvas = d3.select("#canvas").append("svg").attr("width", 500).attr("height", 500);
 
+// path play
+const dir = d3.path(__dirname, '');
+console.log(dir);
+
+// sliding shapes
 const startX = math.rand(150, 350);
 const startY = math.rand(150, 350);
 const rectangle = canvas.append('rect')
@@ -16,3 +22,9 @@ const triangle = canvas.append('path')
     .attr('fill', colors.rand())
     .attr('d', `M ${startX} ${startY} L ${startX - 50} ${startY + 75} H ${startX + 50} Z`);
 triangle.transition().duration(math.rand(4444,8888)).attr('transform', `translate(${math.rand(178,222)}, 0)`)
+
+// text play
+canvas.append('text')
+    .attr('x', math.rand(0,400)).attr('y', math.rand(0,400))
+    .text(dir.toString())
+    // .attr('transform', `rotate(${math.rand(0,360)})`)
