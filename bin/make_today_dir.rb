bcc6918f303882df108f07914be6040273955407
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-07-26 15:36:29 -0700

    Updates make_today_dir script to conditionally create new month dir when necessary

--- bin/make_today_dir.rb
+++ bin/make_today_dir.rb
@@ -1,8 +1,13 @@
 require "date"
 
-current_year = Date.today.year
-current_date = Date.today.strftime("%m-%d")
 code_studies_dir_name = Dir.pwd
-today_dir_name = "#{code_studies_dir_name}/#{current_year}/#{current_date}"
 
-Dir.mkdir(today_dir_name) unless Dir.exist?(today_dir_name)
+current_year = Date.today.year
+current_month = Date.today.strftime("%m")
+current_date = Date.today.strftime("%m-%d")
+
+this_month_directory_name = "#{code_studies_dir_name}/#{current_year}/#{current_month}"
+Dir.mkdir(this_month_directory_name) if !Dir.exist?(this_month_directory_name)
+
+today_directory_name = "#{this_month_directory_name}/#{current_date}"
+Dir.mkdir(today_directory_name) unless Dir.exist?(today_directory_name)
